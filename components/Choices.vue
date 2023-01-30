6e6657ec5402ca90c7546b56b8586a899a2745a8
Christopher Gonçalves <chrissgon.dev@gmail.com>
2023-01-29 21:11:51 -0300

    feat: :sparkles: adjusts

--- components/Choices.vue
+++ components/Choices.vue
@@ -16,13 +16,17 @@
       </Badge>
     </Tooltip>
 
-    <span v-if="!isSelected" class="absolute rotate-45 top-2 right-4">👆</span>
+    <span
+      v-if="!isSelected"
+      class="hand absolute scale-1 rotate-45 top-4 right-5"
+      >👆</span
+    >
 
     <span
       v-text="text"
       @click="show"
       :class="{ isSelected }"
-      class="answer relative text-slate-500 text-center cursor-pointer px-3 font-medium [&.isSelected]:after:!hidden after:content-[''] after:flex after:w-2 after:h-2 after:absolute after:top-1 after:right-2 after:rounded-full after:bg-amber-500 after:animate-ping"
+      class="answer relative text-slate-500 text-center cursor-pointer px-3 font-medium [&.isSelected]:after:!hidden after:content-[''] after:hidden after:w-2 after:h-2 after:absolute after:top-1 after:right-2 after:rounded-full after:bg-amber-500 after:animate-ping after:-z-10"
     ></span>
   </div>
 </template>
@@ -76,5 +80,46 @@ watch(
   width: 100%;
   height: 100%;
   cursor: no-drop;
+}
+
+.hand {
+  animation: move 5s infinite;
+}
+.answer {
+  animation: click 5s infinite;
+}
+
+@keyframes click {
+  to,
+  from,
+  45%,
+  55% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(0.8);
+  }
+}
+@keyframes move {
+  to,
+  from,
+  35%,
+  65% {
+    top: 1rem;
+    right: 1.25rem;
+    transform: scale(0) rotate(45deg);
+  }
+  40%,
+  60% {
+    top: 1rem;
+    right: 1.25rem;
+    transform: scale(1) rotate(45deg);
+  }
+  45%,
+  55% {
+    top: 0.5rem;
+    right: 1rem;
+    transform: scale(1) rotate(45deg);
+  }
 }
 </style>
