5b08069cf2291ca98656d1a12d9b2b2929285422
Christopher Gonçalves <chrissgon.dev@gmail.com>
2023-02-09 12:22:57 -0300

    feat: :sparkles: carrer page

--- app.vue
+++ app.vue
@@ -5,7 +5,7 @@
   >
     <!-- <BackgroundParallax /> -->
     <section
-      class="App scrollbar w-full h-screen relative overflow-x-hidden px-5 sm:px-10"
+      class="App scrollbar w-full relative overflow-x-hidden px-5 sm:px-10"
     >
       <Header class="pt-10 pb-36" />
       <div class="-mt-28">
